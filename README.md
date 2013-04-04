34faa343e97af0c489d4601fb5ca85269d542ecd
Michel Kuhlmann <michel@kuhlmanns.info>
2013-04-04 13:19:40 +0200

    rudimentary working

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-This is a Julia version of [configurator](https://www.github.com/bos/configurator).
+This is a rudimentary Julia version of a subset of
+[configurator](https://www.github.com/bos/configurator).
+
+The main difference is the way string interpolation works so:
+
+* there is function interpolation eg `a = reverse("abc")` 
+
+Besides that, there is currently no reloading of configuration-files: so it is
+a sole reader.
+
+Already implemented features
+----------------------------
+
+* rudimentary file-reading
+* `import` statements
+* expression interpolation
+
+ToDo
+----
+
+* more flexible syetax 
+* values, defined above in the config-file, should be accessable through `cfg` eg
+
+    a = 1
+    b = cfg["a"] + 2
+
+Example
+-------
+
+    uno = 1
+    dos = 2
+    ll  = [1,2,3]
+    bb  = reverse("abc")
+    tres {
+       a = "da"
+       b = "di"
+       quadro {
+       	  c = "do"
+    	  d = "la"
+       }
+    x {
+       import "test2.cfg"
+    }
+    }
 
--- src/Configurator.jl
+++ src/Configurator.jl
@@ -1,12 +1,50 @@
 module Configurator
-    function readConfig(con::IOStream)
-    end 
 
-    function readConfig(f::String)
-        con = open(f, "r")
-        v = readConfig(con)
-        close(con)
-        v
+function parseAttr(l)
+    (k,v) = map(strip, (split(l, "=")))
+    {k => eval(parse(v))}
+end
+
+function parseList(l)  
+    (k,v) = map(strip, (split(l, "=")))
+    {k => eval(parse(v))}
+end
+
+function parseBlockContents(con)
+    cfg = Dict{Any,Any}()
+    while true
+        line = readline(con) 
+        if line == ""  || line == "}"
+            break
+        end
+        line = chomp(line)
+        line = strip(replace(line, r"\#.*", "")) # remove comments
+        if ismatch(r"=", line)
+            merge!(cfg,parseAttr(line))
+        elseif ismatch(r"\[", line)
+            merge!(cfg, parseList(line))
+        elseif ismatch(r"\{", line)
+            k = strip(split(line, "\{")[1])
+            merge!(cfg, {k => parseBlockContents(con)})
+        elseif ismatch(r"import", line)
+            merge!(cfg,readConfig(eval(parse(split(line)[2]))))
+        else
+            continue
+        end
     end
-    export readConfig
+    cfg
+end
+
+function readConfig(con::IOStream)
+    parseBlockContents(con)
+end 
+
+function readConfig(f::String)
+    con = open(f, "r")
+    v = readConfig(con)
+    close(con)
+    v
+end
+export readConfig
+
 end
